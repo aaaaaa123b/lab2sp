47a1afcfaa3a638e1c7a7c6fc4855db47a2ca440
Diana Harlap <dianaharlap004@gmail.com>
2023-10-23 15:31:52 +0300

    Create window

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,10 +1,90 @@
 ﻿
-#include <iostream>
+#pragma warning (disable: 28251)
+
 #include <windows.h>
 
-int main()
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+
+using namespace std;
+
+
+VOID OnPaint(HWND hWnd, LONG clientWidth, LONG clientHeight)
 {
-    std::cout << "Hello World!\n";
+    
 }
 
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
+{
+
+    HWND                hWnd;
+    MSG                 msg;
+    WNDCLASS            wndClass;
+
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc = WndProc;
+    wndClass.cbClsExtra = 0;
+    wndClass.cbWndExtra = 0;
+    wndClass.hInstance = hInstance;
+    wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndClass.lpszMenuName = NULL;
+    wndClass.lpszClassName = TEXT("GettingStarted");
+
+    RegisterClass(&wndClass);
+
+    hWnd = CreateWindow(
+        TEXT("GettingStarted"),   // window class name
+        TEXT("Lab2"),             // window caption
+        WS_OVERLAPPEDWINDOW,      // window style
+        CW_USEDEFAULT,            // initial x position
+        CW_USEDEFAULT,            // initial y position
+        CW_USEDEFAULT,            // initial x size
+        CW_USEDEFAULT,            // initial y size
+        NULL,                     // parent window handle
+        NULL,                     // window menu handle
+        hInstance,                // program instance handle
+        NULL);                    // creation parameters
+
+    ShowWindow(hWnd, iCmdShow);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
+    WPARAM wParam, LPARAM lParam)
+{
+    static LONG  clientWidth = 800;
+    static LONG  clientHeight = 600;
+
+    switch (message)
+    {
+    case WM_PAINT:
+    {
+        OnPaint(hWnd, clientWidth, clientWidth);
+        return 0;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_SIZE:
+        clientWidth = LOWORD(lParam);
+        clientHeight = HIWORD(lParam);
+        return 0;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+}
 
