39ff070802d1e81ec9a47e7db6e7f1ad0f4003ad
Diana Harlap <dianaharlap004@gmail.com>
2023-10-24 04:27:54 +0300

    Add paint and load metods

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,20 +1,114 @@
-﻿
-#pragma warning (disable: 28251)
+﻿#pragma warning (disable: 28251)
 
 #include <windows.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+
 
 #ifndef UNICODE
 #define UNICODE
 #endif 
 
+#define COLS 4
+#define ROWS 3
 
 using namespace std;
 
+vector<wstring> strings;
 
 VOID OnPaint(HWND hWnd, LONG clientWidth, LONG clientHeight)
 {
-    
+    int padding = 10;
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hWnd, &ps);
+
+    INT colWidth = (INT)(clientWidth / COLS);
+    INT* rowHeights = new INT[ROWS + 1];
+
+    RECT cellRect = RECT();
+    int maxRowHeight = 0;
+    int currRowHeight = 0;
+    int stringIndex = 0;
+    int topRowY = 0;
+    for (int row = 0; row < ROWS; row++)
+    {
+        maxRowHeight = 10;
+        int leftColumnX = 0;
+        int rightColumnX = colWidth;
+        for (int col = 0; col < COLS; col++)
+        {
+            cellRect.left = leftColumnX + padding;
+            cellRect.right = rightColumnX - padding;
+            cellRect.top = topRowY + padding;
+            cellRect.bottom = topRowY + 1000;
+
+            if (stringIndex < strings.size())
+            {
+                currRowHeight = DrawText(hdc, strings.at(stringIndex++).c_str(), -1, &cellRect, DT_WORDBREAK | DT_TOP);
+            }
+
+            if (currRowHeight > maxRowHeight)
+            {
+                maxRowHeight = currRowHeight;
+            }
+
+            leftColumnX += colWidth;
+            rightColumnX += colWidth;
+        }
+
+        if (row == 0)
+        {
+            rowHeights[row] = maxRowHeight + padding + padding;
+        }
+        else
+        {
+            rowHeights[row] = rowHeights[row - 1] + maxRowHeight + padding + padding;
+        }
+        topRowY += maxRowHeight + padding + padding;
+    }
+
+    MoveToEx(hdc, 0, 0, NULL);
+    LineTo(hdc, clientWidth, 0);
+
+    for (int col = 1; col < COLS; col++)
+    {
+        MoveToEx(hdc, col * colWidth, 0, NULL);
+        LineTo(hdc, col * colWidth, clientHeight);
+    }
+
+    for (int row = 0; row < ROWS; row++)
+    {
+        MoveToEx(hdc, 0, rowHeights[row], NULL);
+        LineTo(hdc, clientWidth, rowHeights[row]);
+    }
+
+    EndPaint(hWnd, &ps);
 }
+
+
+void LoadTextFromFile(const wstring& filename) {
+    wifstream inputFile(filename);
+    wstring line;
+    wstring text;
+
+    if (inputFile.is_open()) {
+        while (getline(inputFile, line)) {
+            
+            line.erase(remove_if(line.begin(), line.end(), ::iswspace), line.end());
+            text += line + L" ";
+        }
+        inputFile.close();
+        strings.push_back(text);
+    }
+    else {
+        // Обработка ошибки открытия файла
+        wcerr << L"Ошибка открытия файла: " << filename << endl;
+    }
+}
+
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -68,14 +162,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
 {
     static LONG  clientWidth = 800;
     static LONG  clientHeight = 600;
+ 
 
     switch (message)
     {
-    case WM_PAINT:
+    case WM_CREATE:
     {
-        OnPaint(hWnd, clientWidth, clientWidth);
+        LoadTextFromFile(L"D:\\сп\\lab2\\file1.txt");
+        LoadTextFromFile(L"D:\\сп\\lab2\\file2.txt");
         return 0;
     }
+    case WM_PAINT:
+    {
+    
+        OnPaint(hWnd, clientWidth, clientWidth);
+  
+        return 0;
+    }
+    
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
